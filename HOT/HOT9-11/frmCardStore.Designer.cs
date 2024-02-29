93f54029c603f3c1e03a2af34fdd9e49925df2bc
Erin Vinas <Erin_VinasLayne@insideranken.org>
2024-02-28 23:17:20 -0600

    HOT chapter 9-11

---
+++ HOT/HOT9-11/frmCardStore.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace HOT9_11
+{
+    partial class frmCardStore
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblOccasion = new System.Windows.Forms.Label();
+            this.lblStyle = new System.Windows.Forms.Label();
+            this.cbOccasion = new System.Windows.Forms.ComboBox();
+            this.cbStyle = new System.Windows.Forms.ComboBox();
+            this.gbExtras = new System.Windows.Forms.GroupBox();
+            this.rbCustomMessage = new System.Windows.Forms.RadioButton();
+            this.rbStamp = new System.Windows.Forms.RadioButton();
+            this.rbEnvelope = new System.Windows.Forms.RadioButton();
+            this.txtCustomMessage = new System.Windows.Forms.TextBox();
+            this.lblCost = new System.Windows.Forms.Label();
+            this.pbCardImage = new System.Windows.Forms.PictureBox();
+            this.gbExtras.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCardImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblOccasion
+            // 
+            this.lblOccasion.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.lblOccasion.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOccasion.Location = new System.Drawing.Point(88, 35);
+            this.lblOccasion.Name = "lblOccasion";
+            this.lblOccasion.Size = new System.Drawing.Size(176, 36);
+            this.lblOccasion.TabIndex = 3;
+            this.lblOccasion.Text = "Occasion";
+            this.lblOccasion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblStyle
+            // 
+            this.lblStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.lblStyle.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStyle.Location = new System.Drawing.Point(88, 140);
+            this.lblStyle.Name = "lblStyle";
+            this.lblStyle.Size = new System.Drawing.Size(176, 36);
+            this.lblStyle.TabIndex = 4;
+            this.lblStyle.Text = "Style";
+            this.lblStyle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cbOccasion
+            // 
+            this.cbOccasion.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbOccasion.FormattingEnabled = true;
+            this.cbOccasion.Location = new System.Drawing.Point(313, 35);
+            this.cbOccasion.Name = "cbOccasion";
+            this.cbOccasion.Size = new System.Drawing.Size(268, 39);
+            this.cbOccasion.TabIndex = 0;
+            this.cbOccasion.SelectedIndexChanged += new System.EventHandler(this.cbOccasion_SelectedIndexChanged);
+            // 
+            // cbStyle
+            // 
+            this.cbStyle.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbStyle.FormattingEnabled = true;
+            this.cbStyle.Location = new System.Drawing.Point(313, 140);
+            this.cbStyle.Name = "cbStyle";
+            this.cbStyle.Size = new System.Drawing.Size(268, 39);
+            this.cbStyle.TabIndex = 1;
+            this.cbStyle.SelectedIndexChanged += new System.EventHandler(this.cbStyle_SelectedIndexChanged);
+            // 
+            // gbExtras
+            // 
+            this.gbExtras.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.gbExtras.Controls.Add(this.rbCustomMessage);
+            this.gbExtras.Controls.Add(this.rbStamp);
+            this.gbExtras.Controls.Add(this.rbEnvelope);
+            this.gbExtras.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbExtras.Location = new System.Drawing.Point(91, 227);
+            this.gbExtras.Name = "gbExtras";
+            this.gbExtras.Size = new System.Drawing.Size(220, 208);
+            this.gbExtras.TabIndex = 5;
+            this.gbExtras.TabStop = false;
+            this.gbExtras.Text = "Extras";
+            // 
+            // rbCustomMessage
+            // 
+            this.rbCustomMessage.AutoSize = true;
+            this.rbCustomMessage.Location = new System.Drawing.Point(26, 156);
+            this.rbCustomMessage.Name = "rbCustomMessage";
+            this.rbCustomMessage.Size = new System.Drawing.Size(184, 24);
+            this.rbCustomMessage.TabIndex = 2;
+            this.rbCustomMessage.TabStop = true;
+            this.rbCustomMessage.Text = "Custom Messsage ($0.30)";
+            this.rbCustomMessage.UseVisualStyleBackColor = true;
+            this.rbCustomMessage.CheckedChanged += new System.EventHandler(this.rbCustomMessage_CheckedChanged);
+            // 
+            // rbStamp
+            // 
+            this.rbStamp.AutoSize = true;
+            this.rbStamp.Location = new System.Drawing.Point(26, 101);
+            this.rbStamp.Name = "rbStamp";
+            this.rbStamp.Size = new System.Drawing.Size(111, 24);
+            this.rbStamp.TabIndex = 1;
+            this.rbStamp.TabStop = true;
+            this.rbStamp.Text = "Stamp ($0.50)";
+            this.rbStamp.UseVisualStyleBackColor = true;
+            this.rbStamp.CheckedChanged += new System.EventHandler(this.rbStamp_CheckedChanged);
+            // 
+            // rbEnvelope
+            // 
+            this.rbEnvelope.AutoSize = true;
+            this.rbEnvelope.Location = new System.Drawing.Point(26, 40);
+            this.rbEnvelope.Name = "rbEnvelope";
+            this.rbEnvelope.Size = new System.Drawing.Size(129, 24);
+            this.rbEnvelope.TabIndex = 0;
+            this.rbEnvelope.TabStop = true;
+            this.rbEnvelope.Text = "Envelope ($0.25)";
+            this.rbEnvelope.UseVisualStyleBackColor = true;
+            this.rbEnvelope.CheckedChanged += new System.EventHandler(this.rbEnvelope_CheckedChanged);
+            // 
+            // txtCustomMessage
+            // 
+            this.txtCustomMessage.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCustomMessage.Location = new System.Drawing.Point(91, 466);
+            this.txtCustomMessage.Multiline = true;
+            this.txtCustomMessage.Name = "txtCustomMessage";
+            this.txtCustomMessage.Size = new System.Drawing.Size(490, 243);
+            this.txtCustomMessage.TabIndex = 2;
+            this.txtCustomMessage.TextChanged += new System.EventHandler(this.txtCustomMessage_TextChanged);
+            // 
+            // lblCost
+            // 
+            this.lblCost.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.lblCost.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCost.Location = new System.Drawing.Point(88, 744);
+            this.lblCost.Name = "lblCost";
+            this.lblCost.Size = new System.Drawing.Size(176, 36);
+            this.lblCost.TabIndex = 6;
+            this.lblCost.Text = "Cost:";
+            // 
+            // pbCardImage
+            // 
+            this.pbCardImage.Location = new System.Drawing.Point(627, 35);
+            this.pbCardImage.Name = "pbCardImage";
+            this.pbCardImage.Size = new System.Drawing.Size(610, 745);
+            this.pbCardImage.TabIndex = 7;
+            this.pbCardImage.TabStop = false;
+            // 
+            // frmCardStore
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1259, 807);
+            this.Controls.Add(this.pbCardImage);
+            this.Controls.Add(this.lblCost);
+            this.Controls.Add(this.txtCustomMessage);
+            this.Controls.Add(this.gbExtras);
+            this.Controls.Add(this.cbStyle);
+            this.Controls.Add(this.cbOccasion);
+            this.Controls.Add(this.lblStyle);
+            this.Controls.Add(this.lblOccasion);
+            this.Name = "frmCardStore";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Card Designer";
+            this.Load += new System.EventHandler(this.frmCardStore_Load);
+            this.gbExtras.ResumeLayout(false);
+            this.gbExtras.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCardImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblOccasion;
+        private System.Windows.Forms.Label lblStyle;
+        private System.Windows.Forms.ComboBox cbOccasion;
+        private System.Windows.Forms.ComboBox cbStyle;
+        private System.Windows.Forms.GroupBox gbExtras;
+        private System.Windows.Forms.TextBox txtCustomMessage;
+        private System.Windows.Forms.Label lblCost;
+        private System.Windows.Forms.PictureBox pbCardImage;
+        private System.Windows.Forms.RadioButton rbCustomMessage;
+        private System.Windows.Forms.RadioButton rbStamp;
+        private System.Windows.Forms.RadioButton rbEnvelope;
+    }
+}
+
---
+++ HOT/HOT9-11/frmCardStore.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.Remoting.Messaging;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+
+namespace HOT9_11
+{
+    public partial class frmCardStore : Form
+    {
+        const decimal BIRTHDAYCARDCOST = 2.50m;
+        const decimal GETWELLCARDCOST = 2.75m;
+        const decimal SYMPATHYCARDCOST = 3.00m;
+        const decimal THANKYOUCARDCOST = 3.50m;
+
+        const decimal EnvelopeCost = 0.25m;
+        const decimal StampCost = 0.50m;
+        const decimal CustomMessageCost = 0.25m;
+
+        string[] birthdayCards = { "Birthday Card 1", "Birthday Card 2", "Birthday Card 3" };
+        string[] getWellCards = { "Get Well Card 1", "Get Well Card 2", "Get Well Card 3" };
+        string[] sympathyCards = { "Sympathy Card 1", "Sympathy Card 2", "Sympathy Card 3" };
+        string[] thankYouCards = { "Thank You Card 1", "Thank You Card 2", "Thank You Card 3" };
+
+        string[] birthdayCardImages =
+        {
+            @"C:\Users\rinla\Desktop\HOT911Images\BirthdayCard1.jpg",
+            @"C:\Users\rinla\Desktop\HOT911Images\BirthdayCard2.jpg",
+            @"C:\Users\rinla\Desktop\HOT911Images\BirthdayCard3.jpg"
+        };
+
+        string[] getWellCardImages =
+        {
+            @"C:\Users\rinla\Desktop\HOT911Images\GetWellSoonCard1.jpg",
+            @"C:\Users\rinla\Desktop\HOT911Images\GetWellSoonCard2.jpg",
+            @"C:\Users\rinla\Desktop\HOT911Images\GetWellSoonCard3.jpg"
+        };
+
+        string[] sympathyCardImages =                                       // I tried to resize the photos with paint like last semester however,
+                                                                            // I think they changed how to do it and it only ended up cropping the
+                                                                            // photos rather than resize.
+        {
+            @"C:\Users\rinla\Desktop\HOT911Images\SympathyCard1.jpg",
+            @"C:\Users\rinla\Desktop\HOT911Images\SympathyCard2.jpg",
+            @"C:\Users\rinla\Desktop\HOT911Images\SympathyCard3.jpg"
+        };
+
+        string[] thankYouCardImages =
+        {
+            @"C:\Users\rinla\Desktop\HOT911Images\ThankYouCard1.jpg",
+            @"C:\Users\rinla\Desktop\HOT911Images\ThankYouCard2.png",
+            @"C:\Users\rinla\Desktop\HOT911Images\ThankYouCard3.jpg"
+        };
+
+        decimal totalCost = 0.00m;
+
+        public frmCardStore()
+        {
+            InitializeComponent();
+
+            txtCustomMessage.TextChanged += txtCustomMessage_TextChanged;
+
+            txtCustomMessage.KeyPress += txtCustomMessage_KeyPress;
+
+            cbOccasion.Items.Add("Birthday");
+            cbOccasion.Items.Add("Get Well Soon");
+            cbOccasion.Items.Add("Sympathy");
+            cbOccasion.Items.Add("Thank You");
+
+            rbCustomMessage.CheckedChanged += rbCustomMessage_CheckedChanged;
+        }
+
+        private void cbOccasion_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            HandleOccasionChange();
+        }
+
+        private void HandleOccasionChange()
+        {
+            cbStyle.Items.Clear();
+            cbStyle.SelectedIndex = -1;
+
+            switch (cbOccasion.SelectedIndex)
+            {
+                case -1:
+                    return;
+
+                case 0:
+                    for (int lcv = 0; lcv < birthdayCards.Length; ++lcv)
+                    {
+                        cbStyle.Items.Add(birthdayCards[lcv]);
+                    }
+                    break;
+
+                case 1:
+                    for (int lcv = 0; lcv < getWellCards.Length; ++lcv)
+                    {
+                        cbStyle.Items.Add(getWellCards[lcv]);
+                    }
+                    break;
+
+                case 2:
+                    for (int lcv = 0; lcv < sympathyCards.Length; ++lcv)
+                    {
+                        cbStyle.Items.Add(sympathyCards[lcv]);
+                    }
+                    break;
+
+                case 3:
+                    for (int lcv = 0; lcv < thankYouCards.Length; ++lcv)
+                    {
+                        cbStyle.Items.Add(thankYouCards[lcv]);
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private void cbStyle_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            totalCost = 0.00m;
+
+            HandleStyleChange();
+
+            UpdateCost();
+        }
+
+        private void HandleStyleChange()
+        {
+            if (cbOccasion.SelectedIndex == -1)
+            {
+                MessageBox.Show("You Must Choose An Occasion",
+                            "NO OCCASION SELECTED",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information);
+                return;
+            }
+
+            if (cbStyle.SelectedIndex == -1)
+            {
+                MessageBox.Show("You Must Choose A Style",
+                            "NO STYLE SELECTED",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information);
+                return;
+            }
+
+            if (!rbCustomMessage.Checked)
+            {
+                txtCustomMessage.Enabled = false;
+            }
+
+            switch (cbOccasion.SelectedIndex)
+            {
+                case -1:
+                    return;
+
+                case 0:
+                    totalCost += BIRTHDAYCARDCOST;
+
+                    pbCardImage.Image =
+                        System.Drawing.Image.FromFile(birthdayCardImages[cbStyle.SelectedIndex]);
+                    break;
+
+
+                case 1:
+                    totalCost += GETWELLCARDCOST;
+
+                    pbCardImage.Image =
+                        System.Drawing.Image.FromFile(getWellCardImages[cbStyle.SelectedIndex]);
+                    break;
+
+                case 2:
+                    totalCost += SYMPATHYCARDCOST;
+
+                    pbCardImage.Image =
+                        System.Drawing.Image.FromFile(sympathyCardImages[cbStyle.SelectedIndex]);
+                    break;
+
+                case 3:
+                    totalCost += THANKYOUCARDCOST;
+
+                    pbCardImage.Image =
+                        System.Drawing.Image.FromFile(thankYouCardImages[cbStyle.SelectedIndex]);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private void frmCardStore_Load(object sender, EventArgs e)
+        {
+            txtCustomMessage.KeyPress += txtCustomMessage_KeyPress;
+        }
+
+        private void rbCustomMessage_CheckedChanged(object sender, EventArgs e)
+        {
+            txtCustomMessage.Enabled = rbCustomMessage.Checked;
+            UpdateCost();
+        }
+
+        private void rbEnvelope_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateCost();
+        }
+
+        private void rbStamp_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateCost();
+        }
+
+        private void UpdateCost()
+        {
+            decimal baseCost = 0m;
+
+            if (cbStyle.SelectedItem != null)
+            {
+                string selectedStyle = cbStyle.SelectedItem.ToString();
+
+
+                switch (selectedStyle)
+                {
+                    case "Birthday Card 1":
+                    case "Birthday Card 2":
+                    case "Birthday Card 3":
+                        baseCost = BIRTHDAYCARDCOST;
+                        break;
+                    case "Get Well Card 1":
+                    case "Get Well Card 2":
+                    case "Get Well Card 3":
+                        baseCost = GETWELLCARDCOST;
+                        break;
+                    case "Sympathy Card 1":
+                    case "Sympathy Card 2":
+                    case "Sympathy Card 3":
+                        baseCost = SYMPATHYCARDCOST;
+                        break;
+                    case "Thank You Card 1":
+                    case "Thank You Card 2":
+                    case "Thank You Card 3":
+                        baseCost = THANKYOUCARDCOST;
+                        break;
+                }
+            }
+
+            decimal extraCost = 0m;
+
+            if (rbEnvelope.Checked)
+            {
+                extraCost += EnvelopeCost;
+            }
+
+            if (rbStamp.Checked)
+            {
+                extraCost += StampCost;
+            }
+
+            if (rbCustomMessage.Checked)
+            {
+                extraCost += CustomMessageCost;
+            }
+
+            totalCost = baseCost + extraCost;
+
+            lblCost.Text = $"Total Cost: ${totalCost}"; // I can not get the total cost to show up on the form. I think I have the correct code,
+                                                        // I'm not finding any errors in my code anywhere.
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void txtCustomMessage_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCustomMessage_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                string customMessage = txtCustomMessage.Text;
+
+                if (!string.IsNullOrEmpty(customMessage))
+                {
+                    MessageBox.Show(customMessage, "Custom Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+    }
+}
